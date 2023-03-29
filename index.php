ef905b6315f5d94a675b8df6d522a3faf6569a53
Lucas Emanoel Gitirana <gitiranalucas5@gmail.com>
2023-03-29 10:15:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -31,27 +31,4 @@
         var_dump($pessoa);
     echo "</pre>";
 
-
-    // unset($pessoa2);
-    //echo "<p>Número de pessoas: ". Pessoa::getNumDePessoas(). "</p>";
-
-    
-    /* $pessoa1->nome = "Lucas";
-    $pessoa1->cpf = "049.695.200-54"; */
-
-    /* $pessoa1->setNome("Lucas Emanoel");
-    $pessoa1->setCpf("04969520054");
-    $pessoa1->setIdade(19); */
-    
-    /* echo "<p>Nome: $pessoa1->nome </p>";
-    echo "<p>CPF: $pessoa1->cpf</p>";
-    echo "<hr>"; */
-
-    
-
-    /* echo "<p>Nome: {$pessoa2->getNome()} </p>";
-    echo "<p>CPF: {$pessoa2->getCpf()} </p>";
-    echo "<p>Idade: {$pessoa2->getIdade()} </p>";
-    echo "<hr>"; */
-
     
